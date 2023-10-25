9cb16c9a5c72f65827164d8ceacae47d3202888c
DEVair Martins <devair.martins@gmail.com>
2023-10-24 22:24:42 -0300

    primeiro

--- App.js
+++ App.js
@@ -1,20 +1,23 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from "react";
+import { View, Text } from "react-native";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+function App(){
+  return(
+    <View>
+      <Text>Jesus Cristo é o Senhor</Text>
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+// class App extends Component {
+//   render() {
+//     return (
+//       <View>
+//         <Text>Jesus Cristo é o Senhor</Text>
+//         <Text>Ta gostosinho.</Text>
+//       </View>
+//     );
+//   }
+// }
+
+export default App;
